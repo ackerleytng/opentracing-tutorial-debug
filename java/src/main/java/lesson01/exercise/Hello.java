dc40408a3f78886a7d01f97ead94440f357d6852
Ackerley Tng <ackerleytng@gmail.com>
2018-09-04 07:38:51 +0800

    Commit to share for debugging

--- java/src/main/java/lesson01/exercise/Hello.java
+++ java/src/main/java/lesson01/exercise/Hello.java
@@ -1,4 +1,36 @@
 package lesson01.exercise;
 
+import io.jaegertracing.Configuration;
+
+import io.opentracing.Tracer;
+import io.opentracing.Span;
+
 public class Hello {
+    private final Tracer tracer;
+
+    public static Tracer initTracer(String service) {
+        return Configuration.fromEnv(service).getTracer();
+    }
+
+    private Hello(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    private void sayHello(String helloTo) {
+        Span span = tracer.buildSpan("say-hello").startManual();
+
+        String helloStr = String.format("Hello, %s!", helloTo);
+        System.out.println(helloStr);
+
+        span.finish();
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            throw new IllegalArgumentException("Expecting one argument");
+        }
+        String helloTo = args[0];
+        new Hello(initTracer("hello-world")).sayHello(helloTo);
+    }
+
 }
